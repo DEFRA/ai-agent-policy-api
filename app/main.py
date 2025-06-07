b618a996b874c4e6fa28db1777357c9dec95271c
Anthony Scott-Hodgetts <ash@torchtech.uk>
2025-06-07 23:14:08 +0100

    Fixed import issue

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from app.common.tracing import TraceIdMiddleware
 from app.config import config
 from app.health.router import router as health_router
 from app.policy.router import router as policy_router
-from app.utils.storage import check_pq_ids, check_storage
+from app.utils.storage import check_storage, get_pq_ids
 
 logger = getLogger(__name__)
 
@@ -28,7 +28,7 @@ async def lifespan(_: FastAPI):
 
     question_store, answer_store = await check_storage()
 
-    pq_ids = await check_pq_ids()
+    pq_ids = await get_pq_ids()
     print(f"Retrieved {len(pq_ids)} PQ ids.")
 
 

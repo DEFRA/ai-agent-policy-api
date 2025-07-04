e39f1bd55674dc75dc7399f3b020c5ed95aa04d7
Anthony Scott-Hodgetts <ash@torchtech.uk>
2025-07-04 18:41:25 +0100

    Fixed timestamp retrieval.

--- app/common/sync_mongo.py
+++ app/common/sync_mongo.py
@@ -94,6 +94,7 @@ def list_timestamp_data(collection_name: str = "semantic_output"):
     for item in cursor:
         content = item.get("content",{})
         result = content.get("data",[])
+        print(result.keys())
         key = result.get("_id","")
         timestamp = result.get("timestamp","")
         timestamps.append((key,timestamp))
